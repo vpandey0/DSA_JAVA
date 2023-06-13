3b1849260cb5ccd702c55e69f5656a00afac9f6f
vpandey0 <vpandey2606@gamil.com>
2023-06-13 19:13:56 +0530

    commit 2

--- Dsa_JAVA/src/array/ArrayQues.java
+++ Dsa_JAVA/src/array/ArrayQues.java
@@ -2,9 +2,33 @@ package array;
 
 public class ArrayQues {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		public static void pushZerosAtEnd(int[] arr) {
+	    	//Your code goes here
+	        int i=0,j=0;
+	     //Push Zeros To Ends   
+	        while(i<arr.length){
+	            if(arr[i]!=0){
+	              int temp=arr[i];
+	                arr[i]=arr[j];
+	                arr[j]=temp;
+	                i++;
+	                j++;  
+	            }
+	            else{
+	                i++;
+	            }
+	            
+	        }  
+	        
+	    }
+		public static void main(String []args) {
+			
+			int arr[]= {5,5,0,0,1,2};
+			pushZerosAtEnd(arr);
+		for(int i=0;i<arr.length;i++) {
+			System.out.println(arr[i]);
+		}
+			
 	}
 
 }
---
+++ Dsa_JAVA/src/praticeCN/quesType2.java
@@ -0,0 +1,75 @@
+package praticeCN;
+import java.util.*;
+public class quesType2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Scanner sc=new Scanner(System.in);
+//		int n=sc.nextInt();
+//		int num1[]=new int[n];
+//		
+//		for(int i=0;i<n;i++) {
+//			num1[i]=sc.nextInt();
+//		}
+//		
+//		int num2[]=new int[n];
+//		
+//		for(int i=0;i<n;i++) {
+//			num2[i]=sc.nextInt();
+//		}
+//		int target=sc.nextInt();
+      
+      
+//----Q- CHECK NUMBER SEQUENCE----------
+		
+//		 int a[]=new int[n];
+//			for(int i=0;i<n;i++) {
+//				int k=s.nextInt();
+//				a[i]=k;
+//			}
+//	        
+//	        boolean isdec=true;
+//	        
+//	        boolean ans=true;
+//	        
+//	        for(int i =0;i<n-1;i++) {
+//				if(a[i]==a[i+1]) {
+//					ans=false;
+//					break;
+//				}
+//				else if(a[i]<a[i+1]) {
+//					isdec=false;
+//				}
+//				else if(a[i]>a[i+1] && isdec==false) {
+//					ans=false;
+//				}
+//				
+//			}
+//			System.out.println(ans);
+//		
+		
+		String str1="abcd";
+		String str2="abdc";
+		
+//		char []ch=new char[26];
+//        if(str1.length()!=str2.length()){
+//            System.out.println("False");
+//        }
+//        for(int i=0;i<ch.length;i++){
+//          ch[str1.charAt(i)-'a']++;
+//          ch[str2.charAt(i)-'a']--;
+//        }
+        int c=0;
+		for(int i=0;i<str1.length();i++) {
+			if(str1.charAt(i)!=str2.charAt(i)) {
+				System.out.println(true);
+				c++;
+				break;
+				
+			}
+			c=0;
+		}
+		System.out.println(c);
+	       
+}
+
+}
