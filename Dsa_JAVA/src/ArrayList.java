25c94b9e961557d6348e58f32de67462fac7d7f9
vpandey0 <vpandey2606@gamil.com>
2023-06-25 13:44:38 +0530

    ..

--- Dsa_JAVA/src/ArrayList.java
+++ Dsa_JAVA/src/ArrayList.java
@@ -1,9 +1,11 @@
-
+import java.util.ArrayList
+;
 public class ArrayList {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
+       ArrayList<Integer> arrList=new ArrayList<>();
+       
+       }
 
 }
